e293f6a726bafb4c07edd5d404d4b83e94496115
Rafa <rafaelcbs1@hotmail.com>
2023-04-03 21:32:31 -0300

    second commit

--- src/main/java/br/univille/dentista/controller/HomeController.java
+++ src/main/java/br/univille/dentista/controller/HomeController.java
@@ -3,13 +3,18 @@ package br.univille.dentista.controller;
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.servlet.ModelAndView;
+import java.util.HashMap;
+import org.springframework.web.bind.annotation.RequestMapping;
 
 @Controller
+@RequestMapping("/")
 public class HomeController {
-    
+
     @GetMapping
     public ModelAndView index() {
-        return new ModelAndView("home/index");
+        HashMap<String, String> map = new HashMap<>();
+        map.put("apelido", "apelido");
+        map.put("nome", "Zeferino");
+        return new ModelAndView("home/index", map);
     }
-
 }
